cb1c30834e83d89793c8f39d6d336b3d361c9efe
ameerhamzagcu-cmyk <ameerhamza.gcu@gmail.com>
2026-01-05 16:42:48 +0500

    Add files via upload

---
+++ Header.tsx
@@ -0,0 +1,2 @@
+import Link from 'next/link';
+export function Header() { return <header><h1>ShaadiSpot</h1></header> }
---
+++ page.tsx
@@ -0,0 +1,2 @@
+import { Header } from './components/Header';
+export default function Home() { return <main><Header/><h1>Homepage</h1></main> }
